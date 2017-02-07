4f16a4d5e479c1d19d707fa3dcda9201389a1748
Le2u <lijiacheng@lijiachengs-MacBook-Pro.local>
2017-02-08 05:56:43 +0800

    test again

---
+++ main.c
@@ -0,0 +1,27 @@
+//
+//  main.c
+//  test
+//
+//  Created by lijiacheng on 2017/2/8.
+//  Copyright © 2017年 lijiacheng. All rights reserved.
+//
+
+#include <stdio.h>
+
+int max(int a , int b )
+{
+    if(a>b){
+        return a;
+    }else{
+        return b;
+    }
+}
+
+int main()
+{
+    int a1 = 26;
+    int a2 = 23;
+    int maxnum = max(a1,a2);
+    printf("The maxnum is %d \n",maxnum);
+    return 0;
+}
